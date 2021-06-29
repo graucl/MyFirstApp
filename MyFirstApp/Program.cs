d9ed33882d0e7768db139d99d0eca4948a4eb58a
Lluís Grau <lluis.grau@col.vueling.com>
2021-06-29 21:26:11 +0200

    Foreach implementation with Arraylist example. Closes #1

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using MyFirstApp.ForeachLoop;
+using System;
 using System.Windows.Forms;
 
 namespace MyFirstApp
@@ -16,7 +14,7 @@ namespace MyFirstApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmForEach());
         }
     }
 }
