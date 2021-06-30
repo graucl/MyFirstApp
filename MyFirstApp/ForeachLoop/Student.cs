a3e53ad04136007c561707e1a480d6d7f6c421c3
Lluís Grau <lluis.grau@col.vueling.com>
2021-06-30 13:00:44 +0200

    Do While, Guid and contructors of Student class implemented

--- MyFirstApp/ForeachLoop/Student.cs
+++ MyFirstApp/ForeachLoop/Student.cs
@@ -1,12 +1,59 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MyFirstApp.ForeachLoop
 {
     public class Student
     {
+        public Guid Guid { get; set; }
         public int StudentId { get; set; }
         public String Name { get; set; }
         public String Surname { get; set; }
         public int Age { get; set; }
+
+        // Static variable that must be initialized at run time.
+        // Static means shared.
+        public static int STUDENT_COUNTER;
+
+        // Static constructor is called at most onee time, before any
+        // instance constructor is invoked or member is accessed.
+        // Is single thread and you can use it to create a singleton.
+        static Student()
+        {
+            STUDENT_COUNTER = 0;
+        }
+
+        public Student()
+        {
+            STUDENT_COUNTER += 1;
+            Guid = Guid.NewGuid();
+        }
+
+        public Student(int studentId, string name, string surname, int age) : this()
+        {
+            StudentId = studentId;
+            Name = name;
+            Surname = surname;
+            Age = age;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Student student &&
+                   StudentId == student.StudentId &&
+                   Name == student.Name &&
+                   Surname == student.Surname &&
+                   Age == student.Age;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = -1235398547;
+            hashCode = hashCode * -1521134295 + StudentId.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Name);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Surname);
+            hashCode = hashCode * -1521134295 + Age.GetHashCode();
+            return hashCode;
+        }
     }
 }
---
+++ MyFirstApp/WhileLoop/frmWhile.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace MyFirstApp.WhileLoop
+{
+    partial class frmWhile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtStudentId = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(144, 194);
+            this.txtAge.Margin = new System.Windows.Forms.Padding(2);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(172, 20);
+            this.txtAge.TabIndex = 19;
+            // 
+            // txtStudentId
+            // 
+            this.txtStudentId.Location = new System.Drawing.Point(144, 81);
+            this.txtStudentId.Margin = new System.Windows.Forms.Padding(2);
+            this.txtStudentId.Name = "txtStudentId";
+            this.txtStudentId.Size = new System.Drawing.Size(172, 20);
+            this.txtStudentId.TabIndex = 18;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(144, 154);
+            this.txtSurname.Margin = new System.Windows.Forms.Padding(2);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(172, 20);
+            this.txtSurname.TabIndex = 17;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(144, 118);
+            this.txtName.Margin = new System.Windows.Forms.Padding(2);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(172, 20);
+            this.txtName.TabIndex = 16;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(188, 237);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(73, 23);
+            this.btnSave.TabIndex = 15;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // frmWhile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 341);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtStudentId);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnSave);
+            this.Name = "frmWhile";
+            this.Text = "frmWhile";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtStudentId;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
