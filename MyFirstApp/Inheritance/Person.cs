14b5e7c8a07f3b1f0fb61c480cf26b57a13d1344
Lluís Grau <lluis.grau@col.vueling.com>
2021-07-01 13:13:47 +0200

    Inheritance implementation with polymorphism. closes #7

---
+++ MyFirstApp/Inheritance/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyFirstApp.Inheritance
+{
+    public class Person
+    {
+        public Guid Guid { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public Person()
+        {
+            Guid = Guid.NewGuid();
+        }
+
+        public Person(string name, string surname) : this()
+        {
+            Name = name;
+            Surname = surname;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Person person &&
+                   Guid.Equals(person.Guid) &&
+                   Name == person.Name &&
+                   Surname == person.Surname;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 411991944;
+            hashCode = hashCode * -1521134295 + Guid.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Name);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Surname);
+            return hashCode;
+        }
+    }
+}
