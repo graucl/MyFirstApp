7d32c043f5b5c41555baa56afa5a5bf9a47cd62f
Lluís Grau <lluis.grau@col.vueling.com>
2021-07-01 14:12:47 +0200

    Abstract classes implementation. closes #9

---
+++ MyFirstApp/AbstractClasses/GeeksForGeeks.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyFirstApp.AbstractClasses
+{
+    public abstract class GeeksForGeeks : ICloneable
+    {
+        //public object Clone()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public abstract void gfg();
+
+        public abstract object Clone();
+
+        public string Test()
+        {
+            return "we are testing";
+        }
+
+        public virtual string VirtualMethod()
+        {
+            return "we are in VirtualMethod";
+        }
+    }
+}
